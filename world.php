03063864a59d109b1b5b9c8867497c59e7a88a57
Saaeen-Grant-UWI <saaeengrant.uwi@gmail.com>
2023-11-19 20:07:24 -0500

    code clean up

--- world.php
+++ world.php
@@ -3,14 +3,15 @@ $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
-$input = $_GET["country"];
+$input = htmlspecialchars($_GET["country"]);
+
 $tableHeaders = [];
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
 if(array_key_exists('lookup',  $_GET)) {
-  $tableHeaders = ["name","district","population"];
-  $stmt = $conn->query("SELECT cities.name as name, cities.district as district, cities.population as population FROM cities JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE '%$input%'");
+  $tableHeaders = ["name","district","population",];
+  $stmt = $conn->query("SELECT cities.* FROM cities JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE '%$input%'");
 } else {
   $tableHeaders = ["name","continent","independence_year","head_of_state"];
   $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$input%'");
@@ -18,13 +19,13 @@ if(array_key_exists('lookup',  $_GET)) {
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
-
 ?>
 
-<?php
-?>
-
+<!-- 
+  The implementation of the table allows for the dynamic addition of columns 
+  and their data via the tableHeaders array as long as they exist in the world database.
+  example; you may add country_code to the first implimentation of tableHeaders
+-->
 <table>
   <tr>
     <?php foreach ($tableHeaders as $header) { ?>
